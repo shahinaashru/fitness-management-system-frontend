c400359871f612f377b4e3677ee95cabeb97aba6
Shahina Ashraf <shahinaashru@gmail.com>
2025-12-16 16:56:46 +0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router/dom";
 import { router } from "./routes/router";
 import { ToastContainer } from "react-toastify";
+import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
