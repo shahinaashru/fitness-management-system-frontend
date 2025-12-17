5ef419f15ace5c216345c798f7b87afef4a4b29a
Shahina Ashraf <shahinaashru@gmail.com>
2025-12-17 13:04:54 +0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { RouterProvider } from "react-router/dom";
 import { router } from "./routes/router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { HashRouter } from "react-router-dom";
 function App() {
   return (
-    <HashRouter>
+    <>
       <RouterProvider router={router} />
       <ToastContainer />
-    </HashRouter>
+    </>
   );
 }
 export default App;
